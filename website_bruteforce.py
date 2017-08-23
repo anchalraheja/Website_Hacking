dea9a51d665d3c62e989c7bb0653151a4a7fcf90
#root <anchalpranav@gmail.com>
2017-08-23 21:40:32 +0530

    reading the wordlist and resume function

--- website_bruteforce.py
+++ website_bruteforce.py
@@ -7,4 +7,25 @@ threads=10
 url="http://www.example.com"
 wordlist=""
 user-agent ="Googlebot"
+resume = None
 
+def build_wordlist(wordlist):
+    fileopen= open("wordlist","rb")
+    line=fileopen.readlines()
+    fileopen.close()
+    
+    found_resume = False
+    words = Queue.Queue()
+    
+    for word in line:
+        word = word.rstrip()
+        if resume is not None:
+            if found_resume:
+                words.put(word)
+            else:
+                if word == resume:
+                    found_resume= True
+                    print "Resuming list from %s" % resume
+        else:
+            words.put(word)
+    return words
