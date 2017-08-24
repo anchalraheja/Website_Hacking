3dc3879e19c2533b6530752e92fdc8f6154156ac
#root <anchalpranav@gmail.com>
2017-08-24 18:01:45 +0530

    directory bruteforcer function added

--- website_bruteforce.py
+++ website_bruteforce.py
@@ -29,3 +29,34 @@ def build_wordlist(wordlist):
         else:
             words.put(word)
     return words
+
+def directory_bruteforce(word_queue,extensions=None):
+    
+    while not word_queue.empty():
+        attempt = word_queue.get()
+        attempt_list = []
+        if "." not in attempt:
+            attempt_list.append("/%s/" % attempt)
+        else:
+            attempt_list.append("/%s/" % attempt)
+            
+        if extensions:
+            for extensions in extensions:
+                attempt_list.append("/%s%s" % (attempt,extensions))
+        
+        for brute in attempt_list:
+            url2 = "%s%s" % (url,urllib2.quote(brute))
+            try:
+                headers={}
+                headers["User-Agent"] = user-agent
+                r = urllib2.Request(url,headers= headers)
+                
+                response =urllib2.urlopen(r)
+                if len(response.read()):
+                    print "[%d] => %s" % (response.code,url2)
+                    
+                except urllib2.URLError,e:
+                    if hasattr(e,'code') and e.code != 404:
+                        print  "%d => %s" % (e.code,url)
+                        
+                    pass
