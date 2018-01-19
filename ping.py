0797288073c54ea58187d92273fd4f49b4076061
Anchal Raheja <anchalpranav@gmail.com>
2018-01-19 16:19:41 -0600

    added regex to display IP_Hostname

--- ping.py
+++ ping.py
@@ -1,6 +1,11 @@
 import os
 import commands
-
+import subprocess
+import re 
 f = open("server.txt")
 for user in f.readlines():
-	os.system("ping -n 1 " + user)
+	#os.system("ping -n 1 " + user)
+	out = subprocess.check_output("ping -n 1 " + user, shell=True )
+	regex = r"(\d+)(.)(\d+)(.)(\d+)(.)(\d+)"
+	x = re.search(regex, out)
+	print x.group(0) + "  " + user
