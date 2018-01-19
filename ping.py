933d2c7d269876be88ba14d94b0e4fb0049cf9a9
Anchal Raheja <anchalpranav@gmail.com>
2018-01-19 16:20:11 -0600

    Update ping.py

--- ping.py
+++ ping.py
@@ -4,7 +4,6 @@ import subprocess
 import re 
 f = open("server.txt")
 for user in f.readlines():
-	#os.system("ping -n 1 " + user)
 	out = subprocess.check_output("ping -n 1 " + user, shell=True )
 	regex = r"(\d+)(.)(\d+)(.)(\d+)(.)(\d+)"
 	x = re.search(regex, out)
